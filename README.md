0616f7ac1245f79b8eaccdc27ef48c224ce94605
feuyeux <feuyeux@gmail.com>
2024-09-21 20:12:30 +0800

    u

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hello-spring-ai
+
+## Reference
+
+<https://docs.spring.io/spring-ai/reference/1.0/index.html>
---
+++ src/test/java/org/feuyeux/ai/hello/HelloSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.feuyeux.ai.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
